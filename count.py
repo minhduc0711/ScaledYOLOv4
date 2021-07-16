9caa434cf4664df5a000535161a30a1be9c5e70a
Duc Pham <phamminhduc0711@gmail.com>
2021-07-16 11:00:09 +0700

    Refactor code for ROIs

--- count.py
+++ count.py
@@ -18,21 +18,25 @@ from utils.torch_utils import select_device
 from sort import Sort
 
 
-def draw_line(img, slope=1, intercept=0):
-    xs = np.arange(img.shape[1])
-    ys = (slope * xs + intercept).astype(np.int)
-    keep = ys < img.shape[0]
-    xs = xs[keep]
-    ys = ys[keep]
-    img[ys, xs, :] = (0, 0, 255)
+def filter_objects_by_roi(boxes, img, rois):
+    for roi in rois:
+        slope, intercept, is_upper_plane = roi
+        # Draw a line on the resulting image
+        xs = np.arange(img.shape[1])
+        ys = (slope * xs + intercept).astype(np.int)
+        keep = ys < img.shape[0]
+        xs = xs[keep]
+        ys = ys[keep]
+        img[ys, xs, :] = (0, 0, 255)
 
-
-def filter_objects_by_line(boxes, img, slope=1, intercept=0):
-    # bottom right corners
-    xs_br = boxes[:, 0]
-    ys_br = boxes[:, 3]
-    keep = ys_br > (slope * xs_br + intercept),
-    return boxes[keep]
+        # bottom right corners
+        xs_br = boxes[:, 0]
+        ys_br = boxes[:, 3]
+        # filters object lying on the ROI half-plane
+        sign = -1 if is_upper_plane else 1
+        keep = ys_br < (sign * (slope * xs_br + intercept))
+        boxes = boxes[keep]
+    return boxes
 
 
 if __name__ == "__main__":
@@ -73,10 +77,15 @@ if __name__ == "__main__":
     class_counts = defaultdict(lambda: 0)
     objects_appeared = set()
 
-    # NOTE: Change these for different videos
-    slope = 0.3
-    intercept = 200
+    # NOTE: tracker params
     tracker = Sort(max_age=4, min_hits=3, iou_threshold=0.1)
+
+    # NOTE: Change these ROIs for different videos
+    # (slope, intercept, is_upper_plane)
+    # video: tay_son_input
+    half_planes = [
+        (0.3, 200, False)
+    ]
 
     num_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
     pbar = tqdm(total=num_frames)
@@ -100,7 +109,7 @@ if __name__ == "__main__":
 
         boxes = pred[0]
         boxes[:, :4] = scale_coords(img.shape[2:], boxes[:, :4], img0.shape).round()
-        boxes = filter_objects_by_line(boxes, img0, slope, intercept)
+        boxes = filter_objects_by_roi(boxes, img0, half_planes)
         boxes = tracker.update(boxes.detach().cpu().numpy())
 
         for *xyxy, obj_id, cls_id in boxes:
@@ -121,7 +130,6 @@ if __name__ == "__main__":
                 if args.save_output:
                     logs.append([obj_id, c, vid.get(cv2.CAP_PROP_POS_MSEC)])
 
-        draw_line(img0, slope, intercept)
         count_str = ", ".join(
             [f"{k}: {v}" for k, v in class_counts.items()]
         )
